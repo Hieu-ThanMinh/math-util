7c081f0e59b8fa1e8242a13bb5ce6d5c65dca9db
Hieu-ThanMinh <k1692thanminhhieu@gmail.com>
2020-11-16 17:38:19 +0700

    first upload to repo

--- src/hieu/util/MathUtility.java
+++ src/hieu/util/MathUtility.java
@@ -10,7 +10,8 @@ package hieu.util;
  * @author Admin
  */
 public class MathUtility {
-    //minh se lam class nay falke y chang class Nath  cua JDK
+    //minh se lam class nay falke y chang class Math  cua JDK
+     //ta sẽ là clss fake Math với các hàm static để xài chung chô mọi nơi
     //Math.PI.abs(0 .sqrt() .pow() .sin()
     //cái gì mà là dồ xài chung, ko lưu trữ riêng lẻ info ta sẽ
     //chơi static
@@ -25,13 +26,15 @@ public class MathUtility {
     //ko thêm trả về -1  hàm vs giá trị ko xài
     //học cách ném ngoại lệ luôn 
     //thứ 2 ông viết tiếp, 
-    
+   
     public static long getFactorial(int n) {
         if(n < 0 || n > 20)
          throw new IllegalArgumentException("n must be between 0..20");
+        //throw là ném ra tương đương  app   bị dừng
         if(n == 0 || n==1)
             return 1;
         //ko xài else, có else trừ điểm
+        //ngay chỗ này chính là n = 2..20
        long result = 1;
        for(int i = 2; i <= n; i++){
            result *= i; // nhân dồn vào
@@ -39,4 +42,42 @@ public class MathUtility {
        }
        return result;
     }
+    //test nghĩa là đưa hàm ra xài, gọi với các tham số khâc nhau
+    //ví dụ gọi hàm getFactorial (với các số khác nhau dưa vào)
+    //getF(-5), getF(-1) getF(0), getF(5),  ...
+    
+    //ta gọi là test case, các tình huống cần test,  các tình huống xài hàm 
+    //khi ta test hàm, ta cần quan tâm 2 việc
+    //1. dự kiến hàm trả về giá trị gì khi hàm chậy với 1 dầu vào nào đó
+     // dự kiên shafm trả về giá trị, tui gọi là EXPECTED VALUE
+    //ví dụ: hàm sẽ trả về 120 khi gọi getF(5)
+    //2. hàm khi chạy với 1 dầu vào nào đó, thì nó sẽ ói/ return về 1 value
+    //cái value trả về cảu hàm khi chạy đc gọi là ACTUAL VALUE
+    
+    //NGHÊ TEST CHÍNH LÀ: SO SÁNH COI EXPECTED VALUE CÓ BẰNG VỚI ACTUAL, VALUE HEM?
+    // NẾU CÓ BẰNG, HÀM ĐÚNG CHO CASE/TÌNH HUỐNG NÀY 
+    //NẾU CÓ BẰNG, HÀM TÍNH TOÁN SAI,  HOẶC KÌ VỌNG SAI
+    
+    //HÀM MÀ TỐT THÌ PHẢI LÀ EXCEPTION  == ACTUAL  MỌI TÌNH HUỐNG
+    
+    //NẾU TỐT TA MỚI ĐEM RA XÀI, BÁN, PUBLIC CỘNG ĐÒNG XÀI
+    
+    //LÀM SAO ĐỂ TEST HÀM CÓI CÓ TỐT KO, EXOECTED == ACTUAL 
+    //HAI KĨ THUẬT ĐC ÁP DỤNG 
+    
+    //KĨ THUẬT 1: nhìn bằng mắt và so sánh từng cặp expected vs. actual
+    //          chính là kĩ thuật sout(expected), sout(actual- hàm ói về giá trị khi xài)
+    //kĩ thauatj này đươn giản , dễ làm , nhưng mắc sai sót do có quá nhiều cặp
+    //expected actual cần so sánh bằng mắt
+    
+    //KĨ THUẬT 2: nhìn bằng mắt , ko cần so sánh từng cặp, đẻ máy so sánh giùm luôn
+    
+    
+    
+    
+    //tương đương câu: hàm tui chạy ngon lắm , đúng 99.9%, lâu lâu sai tí
+    //nói câu đó: hàm éo tin cậy đẻ xài
+    //Kỹ THUẬT 2 NÀY DÙNG MÀU SẮC, MÚN LÀM VẬY THÌ PHẢI XÀI THƯ VIỆN BỔ SUNG THÊM 
+    //CHÍNH LÀ FILE.JAR, .DLL ĐC CÙNG CẤP THÊM NGOÀI JDK
+    //Các thư viện
 }
--- src/math/util/MathUtil.java
+++ src/math/util/MathUtil.java
@@ -5,6 +5,7 @@
  */
 package math.util;
 
+import hieu.util.MathUtility;
 import static hieu.util.MathUtility.getFactorial;
 import static hieu.util.MathUtility.*;
 // câu lệnh mới từ JDK8 (5), khai báo sẵn hàm Static,
@@ -20,20 +21,51 @@ public class MathUtil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       //ta test thử hàm của ta 
-       //dùng mắt để so sánh  kết quả thực tế khi chạy và kết quả ta mong đợi
-       long expected = 120; // ta mong nhận 120  nếu gọi hàm 5!
-       long actual = getFactorial(5);
-        System.out.println("Expected:" + expected + "; actual:" + actual);
+//       //ta test thử hàm của ta 
+//       //dùng mắt để so sánh  kết quả thực tế khi chạy và kết quả ta mong đợi
+//       long expected = 120; // ta mong nhận 120  nếu gọi hàm 5!
+//       long actual = getFactorial(5);
+//        System.out.println("Expected:" + expected + "; actual:" + actual);
+//        
+//        //expected: 720 if tính 6!
+//        System.out.println("6!:" + getFactorial(6));
+//        //expected : 1 if tính 0!
+//        System.out.println("0! :" + getFactorial(0));
+//        //expected: IllegalargurmentException if tính -5!
+//        System.out.println("-5! :" + getFactorial(-5));
+//        // in thử PI coi có  giống Math.PI hem?
+//        System.out.println("PI:" + PI);
+
+
+     long expected = 120; // tao hy vọng 120 ói về nếu
+     long actual = MathUtility.getFactorial(5);// tao gọi hàm 5!
+        System.out.println("5!? expected:" + expected + "; actual:"+actual );
+        // dùng cho case tình huống đàu tiên
         
-        //expected: 720 if tính 6!
-        System.out.println("6!:" + getFactorial(6));
-        //expected : 1 if tính 0!
-        System.out.println("0! :" + getFactorial(0));
-        //expected: IllegalargurmentException if tính -5!
-        System.out.println("-5! :" + getFactorial(-5));
-        // in thử PI coi có  giống Math.PI hem?
-        System.out.println("PI:" + PI);
-    }
-    
+        expected = 720; //hy vọng  ói về 720 nếu tao gọi
+        actual = MathUtility.getFactorial(6);
+        System.out.println("6!? expected:" + expected + "; actual:" + actual);
+        
+        System.out.println("0!? expected: 1; actual:" + MathUtility.getFactorial(0));
+            
+        //case4: 
+        //tao kỳ vọng nhận về ngoại lệ IllegalAgurmentException
+        //nếu tao gọi -5!
+        MathUtility.getFactorial(-5);
+        //thấy ngoại lệ mừng vì hàm chạy như thiết kế
+        
+       }
 }
+//cất code lên github
+//cần:
+// nhớ kho trên github, url: hrrps://github.com/ten-minh/ten-kho.git
+//nhớ uername và pass và email vào githiub cảu mình
+//cần tool để đồng bộ code từ máy mình (loca) lên server(GitHub, Gitlab)
+//tool có thể là: cmd, GUI (git desktop, SOurce tree), chính IDE(cmd, click) 
+//vì ta pro ta chơi cmd
+//file đặc biệt để  nói với git tool rằng: ai  lên server ai ở lại local
+//file này đc gọi là .ignoore (thuần text)
+//nó sẽ khác nhau content tùy vào IDE minh xài, ngôn ngữ lập trình minh xài
+//có 1 gã dev cực dễ thương, làm sẵn những file ứng với cái iDE, NNLT rồi
+//xin anh ấy đên về xài http://gitignore.io
+//
